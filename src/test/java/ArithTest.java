724d60c282cc6fcc710122e43d81016d0ec211a4
Lexes Mantiquilla <mystery32120@gmail.com>
2019-09-10 23:40:09 +0100

    Added the correct dependencies

---
+++ src/test/java/ArithTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class ArithTest {
+    @Test
+    void testValidatePrefixOrder() {
+        assertEquals(Arith.validatePrefixOrder(new String[]{"+", "1", "2"}), true);
+        assertEquals(Arith.validatePrefixOrder(new String[]{"+", "1", "2", "-", "4"}), false);
+
+    }
+
+    @Test
+    void testValidatePostfixOrder() {
+        assertEquals(Arith.validatePostfixOrder(new String[]{}), true);
+        assertEquals(Arith.validatePostfixOrder(new String[]{}), false);
+    }
+}
