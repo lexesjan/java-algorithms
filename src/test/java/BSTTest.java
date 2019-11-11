4a969801c74f7357ba86c396907c86d7b39b00ad
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2019-11-11 01:09:56 +0000

    Implement height method and test

--- src/test/java/BSTTest.java
+++ src/test/java/BSTTest.java
@@ -4,105 +4,158 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-//-------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 /**
- *  Test class for Doubly Linked List
+ * Test class for Doubly Linked List
  *
- *  @version 3.1 09/11/15 11:32:15
- *
- *  @author Lexes Jan Mantiquilla
+ * @version 3.1 09/11/15 11:32:15
+ * @author Lexes Jan Mantiquilla
  */
-
 @RunWith(JUnit4.class)
-public class BSTTest
-{
-  
-  //TODO write more tests here.
+public class BSTTest {
 
-  
-  /** <p>Test {@link BST#prettyPrintKeys()}.</p> */
-      
- @Test
- public void testPrettyPrint() {
-     BST<Integer, Integer> bst = new BST<Integer, Integer>();
-     assertEquals("Checking pretty printing of empty tree",
-             "-null\n", bst.prettyPrintKeys());
-      
-                          //  -7
-                          //   |-3
-                          //   | |-1
-                          //   | | |-null
-     bst.put(7, 7);       //   | |  -2
-     bst.put(8, 8);       //   | |   |-null
-     bst.put(3, 3);       //   | |    -null
-     bst.put(1, 1);       //   |  -6
-     bst.put(2, 2);       //   |   |-4
-     bst.put(6, 6);       //   |   | |-null
-     bst.put(4, 4);       //   |   |  -5
-     bst.put(5, 5);       //   |   |   |-null
-                          //   |   |    -null
-                          //   |    -null
-                          //    -8
-                          //     |-null
-                          //      -null
-     
-     String result = 
-      "-7\n" +
-      " |-3\n" + 
-      " | |-1\n" +
-      " | | |-null\n" + 
-      " | |  -2\n" +
-      " | |   |-null\n" +
-      " | |    -null\n" +
-      " |  -6\n" +
-      " |   |-4\n" +
-      " |   | |-null\n" +
-      " |   |  -5\n" +
-      " |   |   |-null\n" +
-      " |   |    -null\n" +
-      " |    -null\n" +
-      "  -8\n" +
-      "   |-null\n" +
-      "    -null\n";
-     assertEquals("Checking pretty printing of non-empty tree", result, bst.prettyPrintKeys());
-     }
+  // TODO write more tests here.
 
-  
-     /** <p>Test {@link BST#delete(Comparable)}.</p> */
-     @Test
-     public void testDelete() {
-         BST<Integer, Integer> bst = new BST<Integer, Integer>();
-         bst.delete(1);
-         assertEquals("Deleting from empty tree", "()", bst.printKeysInOrder());
-         
-         bst.put(7, 7);   //        _7_
-         bst.put(8, 8);   //      /     \
-         bst.put(3, 3);   //    _3_      8
-         bst.put(1, 1);   //  /     \
-         bst.put(2, 2);   // 1       6
-         bst.put(6, 6);   //  \     /
-         bst.put(4, 4);   //   2   4
-         bst.put(5, 5);   //        \
-                          //         5
-         
-         assertEquals("Checking order of constructed tree",
-                 "(((()1(()2()))3((()4(()5()))6()))7(()8()))", bst.printKeysInOrder());
-         
-         bst.delete(9);
-         assertEquals("Deleting non-existent key",
-                 "(((()1(()2()))3((()4(()5()))6()))7(()8()))", bst.printKeysInOrder());
- 
-         bst.delete(8);
-         assertEquals("Deleting leaf", "(((()1(()2()))3((()4(()5()))6()))7())", bst.printKeysInOrder());
- 
-         bst.delete(6);
-         assertEquals("Deleting node with single child",
-                 "(((()1(()2()))3(()4(()5())))7())", bst.printKeysInOrder());
- 
-         bst.delete(3);
-         assertEquals("Deleting node with two children",
-                 "(((()1())2(()4(()5())))7())", bst.printKeysInOrder());
-     }
-     
+  /** Test {@link BST#prettyPrintKeys()}. */
+  @Test
+  public void testPrettyPrint() {
+    BST<Integer, Integer> bst = new BST<Integer, Integer>();
+    assertEquals("Checking pretty printing of empty tree", "-null\n", bst.prettyPrintKeys());
+
+    //  -7
+    //   |-3
+    //   | |-1
+    //   | | |-null
+    bst.put(7, 7); //   | |  -2
+    bst.put(8, 8); //   | |   |-null
+    bst.put(3, 3); //   | |    -null
+    bst.put(1, 1); //   |  -6
+    bst.put(2, 2); //   |   |-4
+    bst.put(6, 6); //   |   | |-null
+    bst.put(4, 4); //   |   |  -5
+    bst.put(5, 5); //   |   |   |-null
+    //   |   |    -null
+    //   |    -null
+    //    -8
+    //     |-null
+    //      -null
+
+    String result =
+        "-7\n"
+            + " |-3\n"
+            + " | |-1\n"
+            + " | | |-null\n"
+            + " | |  -2\n"
+            + " | |   |-null\n"
+            + " | |    -null\n"
+            + " |  -6\n"
+            + " |   |-4\n"
+            + " |   | |-null\n"
+            + " |   |  -5\n"
+            + " |   |   |-null\n"
+            + " |   |    -null\n"
+            + " |    -null\n"
+            + "  -8\n"
+            + "   |-null\n"
+            + "    -null\n";
+    assertEquals("Checking pretty printing of non-empty tree", result, bst.prettyPrintKeys());
+  }
+
+  /** Test {@link BST#delete(Comparable)}. */
+  @Test
+  public void testDelete() {
+    BST<Integer, Integer> bst = new BST<Integer, Integer>();
+    bst.delete(1);
+    assertEquals("Deleting from empty tree", "()", bst.printKeysInOrder());
+
+    bst.put(7, 7);
+    bst.put(8, 8);
+    bst.put(3, 3);
+    bst.put(1, 1);
+    bst.put(2, 2);
+    bst.put(6, 6);
+    bst.put(4, 4);
+    bst.put(5, 5);
+    /*
+            _7_
+          /     \
+        _3_      8
+      /     \
+     1       6
+      \     /
+       2   4
+            \
+             5
+     */
+
+    assertEquals(
+        "Checking order of constructed tree",
+        "(((()1(()2()))3((()4(()5()))6()))7(()8()))",
+        bst.printKeysInOrder());
+
+    bst.delete(9);
+    assertEquals(
+        "Deleting non-existent key",
+        "(((()1(()2()))3((()4(()5()))6()))7(()8()))",
+        bst.printKeysInOrder());
+
+    bst.delete(8);
+    assertEquals("Deleting leaf", "(((()1(()2()))3((()4(()5()))6()))7())", bst.printKeysInOrder());
+
+    bst.delete(6);
+    assertEquals(
+        "Deleting node with single child",
+        "(((()1(()2()))3(()4(()5())))7())",
+        bst.printKeysInOrder());
+
+    bst.delete(3);
+    assertEquals(
+        "Deleting node with two children", "(((()1())2(()4(()5())))7())", bst.printKeysInOrder());
+  }
+
+  @Test
+  public void testHeight() {
+    BST<Integer, Integer> bst = new BST<>();
+
+    assertEquals("Getting height of empty tree", -1, bst.height());
+
+    bst.put(7, 7);
+
+    /*
+     7
+     */
+
+    assertEquals("Getting height of tree with one node", 0, bst.height());
+
+    bst.put(8, 8);
+    bst.put(3, 3);
+
+    /*
+        _7_
+      /     \
+     3       8
+     */
+
+    assertEquals("Getting height of tree of 1", 1, bst.height());
+
+    bst.put(1, 1);
+    bst.put(2, 2);
+    bst.put(6, 6);
+    bst.put(4, 4);
+    bst.put(5, 5);
+
+    /*
+            _7_
+          /     \
+        _3_      8
+      /     \
+     1       6
+      \     /
+       2   4
+            \
+             5
+     */
+
+    assertEquals("Getting height of 4", 4, bst.height());
+  }
 }
-
