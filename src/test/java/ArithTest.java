377a11538302293910f26e2a7be9c850bb8a9530
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-01-11 14:38:19 +0000

    Fix jUnit error
    Methods were not public

--- src/test/java/ArithTest.java
+++ src/test/java/ArithTest.java
@@ -2,20 +2,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-
 public class ArithTest {
-    @Test
-    void testValidatePrefixOrder() {
-        assertTrue(Arith.validatePrefixOrder(new String[]{"+", "1", "2"}));
-        assertTrue(Arith.validatePrefixOrder(new String[]{"+", "*", "-", "1", "2", "3", "-", "10", "+", "3", "/", "6", "3"}));
-        assertFalse(Arith.validatePrefixOrder(new String[]{"+", "1", "2", "-", "4"}));
+  @Test
+  public void testValidatePrefixOrder() {
+    assertTrue(Arith.validatePrefixOrder(new String[] {"+", "1", "2"}));
+    assertTrue(
+        Arith.validatePrefixOrder(
+            new String[] {"+", "*", "-", "1", "2", "3", "-", "10", "+", "3", "/", "6", "3"}));
+    assertFalse(Arith.validatePrefixOrder(new String[] {"+", "1", "2", "-", "4"}));
+  }
 
-    }
-
-    @Test
-    void testValidatePostfixOrder() {
-        assertTrue(Arith.validatePostfixOrder(new String[]{"1", "2", "+"}));
-        assertTrue(Arith.validatePostfixOrder(new String[]{"3", "3", "1", "-", "*", "3", "6", "/", "3", "+", "10", "-", "+"}));
-        assertFalse(Arith.validatePostfixOrder(new String[]{"1", "-", "2", "3", "+"}));
-    }
+  @Test
+  public void testValidatePostfixOrder() {
+    assertTrue(Arith.validatePostfixOrder(new String[] {"1", "2", "+"}));
+    assertTrue(
+        Arith.validatePostfixOrder(
+            new String[] {"3", "3", "1", "-", "*", "3", "6", "/", "3", "+", "10", "-", "+"}));
+    assertFalse(Arith.validatePostfixOrder(new String[] {"1", "-", "2", "3", "+"}));
+  }
 }
