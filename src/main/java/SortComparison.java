69784858b1207010b6eee53ae66f01253725be7f
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-02-04 23:16:23 +0000

    Format code and remove syntax errors

--- src/main/java/SortComparison.java
+++ src/main/java/SortComparison.java
@@ -1,100 +1,81 @@
 // -------------------------------------------------------------------------
 
 /**
- *  This class contains static methods that implementing sorting of an array of numbers
- *  using different sort algorithms.
+ * This class contains static methods that implementing sorting of an array of numbers using
+ * different sort algorithms.
  *
- *  @author
- *  @version HT 2020
+ * @author Lexes Jan Mantiquilla
+ * @version HT 2020
  */
+class SortComparison {
 
- class SortComparison {
+  /**
+   * Sorts an array of doubles using InsertionSort. This method is static, thus it can be called as
+   * SortComparison.sort(a)
+   *
+   * @param a: An unsorted array of doubles.
+   * @return array sorted in ascending order.
+   */
+  static double[] insertionSort(double[] a) {
 
-    /**
-     * Sorts an array of doubles using InsertionSort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order.
-     *
-     */
-    static double [] insertionSort (double a[]){
+    // todo: implement the sort
+    return null;
+  } // end insertionsort
 
-        //todo: implement the sort
-    }//end insertionsort
-	
-	    /**
-     * Sorts an array of doubles using Selection Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order
-     *
-     */
-    static double [] selectionSort (double a[]){
+  /**
+   * Sorts an array of doubles using Selection Sort. This method is static, thus it can be called as
+   * SortComparison.sort(a)
+   *
+   * @param a: An unsorted array of doubles.
+   * @return array sorted in ascending order
+   */
+  static double[] selectionSort(double[] a) {
 
-         //todo: implement the sort
+    // todo: implement the sort
+    return null;
+  } // end selectionsort
 
-    }//end selectionsort
+  /**
+   * Sorts an array of doubles using Quick Sort. This method is static, thus it can be called as
+   * SortComparison.sort(a)
+   *
+   * @param a: An unsorted array of doubles.
+   * @return array sorted in ascending order
+   */
+  static double[] quickSort(double[] a) {
 
-    /**
-     * Sorts an array of doubles using Quick Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order
-     *
-     */
-    static double [] quickSort (double a[]){
-	
-		 //todo: implement the sort
+    // todo: implement the sort
+    return null;
+  } // end quicksort
 
-    }//end quicksort
+  /**
+   * Sorts an array of doubles using iterative implementation of Merge Sort. This method is static,
+   * thus it can be called as SortComparison.sort(a)
+   *
+   * @param a: An unsorted array of doubles.
+   * @return after the method returns, the array must be in ascending sorted order.
+   */
+  static double[] mergeSortIterative(double[] a) {
 
-    /**
-     * Sorts an array of doubles using Merge Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     * @param a: An unsorted array of doubles.
-     * @return array sorted in ascending order
-     *
-     */
-    /**
-     * Sorts an array of doubles using iterative implementation of Merge Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     *
-     * @param a: An unsorted array of doubles.
-     * @return after the method returns, the array must be in ascending sorted order.
-     */
+    // todo: implement the sort
+    return null;
+  } // end mergesortIterative
 
-    static double[] mergeSortIterative (double a[]) {
+  /**
+   * Sorts an array of doubles using recursive implementation of Merge Sort. This method is static,
+   * thus it can be called as SortComparison.sort(a)
+   *
+   * @param a: An unsorted array of doubles.
+   * @return after the method returns, the array must be in ascending sorted order.
+   */
+  static double[] mergeSortRecursive(double[] a) {
 
-		 //todo: implement the sort
-	
-    }//end mergesortIterative
-    
-    
-    
-    /**
-     * Sorts an array of doubles using recursive implementation of Merge Sort.
-     * This method is static, thus it can be called as SortComparison.sort(a)
-     *
-     * @param a: An unsorted array of doubles.
-     * @return after the method returns, the array must be in ascending sorted order.
-     */
-    static double[] mergeSortRecursive (double a[]) {
-    	
+    // todo: implement the sort
+    return null;
+  } // end mergeSortRecursive
 
-    	//todo: implement the sort
-	
-   }//end mergeSortRecursive
-    	
-    
+  public static void main(String[] args) {
 
-
-   
-
-
-    public static void main(String[] args) {
-
-        //todo: do experiments as per assignment instructions
-    }
-
- }//end class
-
+    // todo: do experiments as per assignment instructions
+  }
+} // end class
--- src/test/java/SortComparisonTest.java
+++ src/test/java/SortComparisonTest.java
@@ -4,48 +4,37 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-//-------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 /**
- *  Test class for SortComparison.java
+ * Test class for SortComparison.java
  *
- *  @author
- *  @version HT 2020
+ * @author Lexes Jan Mantiquilla
+ * @version HT 2020
  */
 @RunWith(JUnit4.class)
-public class SortComparisonTest
-{
-    //~ Constructor ........................................................
-    @Test
-    public void testConstructor()
-    {
-        new SortComparison();
-    }
+public class SortComparisonTest {
+  // ~ Constructor ........................................................
+  @Test
+  public void testConstructor() {
+    new SortComparison();
+  }
 
-    //~ Public Methods ........................................................
+  // ~ Public Methods ........................................................
 
-    // ----------------------------------------------------------
-    /**
-     * Check that the methods work for empty arrays
-     */
-    @Test
-    public void testEmpty()
-    {
-    }
+  // ----------------------------------------------------------
+  /** Check that the methods work for empty arrays */
+  @Test
+  public void testEmpty() {}
 
+  // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+  // be executed at least once from at least one test.
 
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
-    // be executed at least once from at least one test.
-
-    // ----------------------------------------------------------
-    /**
-     *  Main Method.
-     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
-     *
-     */
-    public static void main(String[] args)
-    {
-        //TODO: implement this method
-    }
-
+  // ----------------------------------------------------------
+  /**
+   * Main Method. Use this main method to create the experiments needed to answer the experimental
+   * performance questions of this assignment.
+   */
+  public static void main(String[] args) {
+    // TODO: implement this method
+  }
 }
-
