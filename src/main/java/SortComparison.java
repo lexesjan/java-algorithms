1f423fb91daee9656230c91ccd3509718a470e34
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-02-05 00:11:14 +0000

    Implement insertion sort
    Added temporary main line

--- src/main/java/SortComparison.java
+++ src/main/java/SortComparison.java
@@ -1,5 +1,7 @@
 // -------------------------------------------------------------------------
 
+import java.util.Scanner;
+
 /**
  * This class contains static methods that implementing sorting of an array of numbers using
  * different sort algorithms.
@@ -17,7 +19,13 @@ class SortComparison {
    * @return array sorted in ascending order.
    */
   static double[] insertionSort(double[] a) {
-    return null;
+    for (int i = 1; i < a.length; i++) {
+      for (int j = i; j > 0; j--) {
+        if (a[j] < a[j - 1]) swap(a, j - 1, j);
+        else break;
+      }
+    }
+    return a;
   } // end insertionsort
 
   /**
@@ -73,7 +81,19 @@ class SortComparison {
   } // end mergeSortRecursive
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int len = scanner.nextInt();
+    double[] nums = new double[len];
+    for (int i = 0; i < nums.length; i++) {
+      nums[i] = scanner.nextDouble();
+    }
 
+    long start = System.currentTimeMillis();
+    insertionSort(nums);
+    long end = System.currentTimeMillis();
+    System.out.printf("Finished in %d ms \n", end - start);
+    System.out.println(isSorted(nums));
+    scanner.close();
     // todo: do experiments as per assignment instructions
   }
 
@@ -82,5 +102,12 @@ class SortComparison {
     double temp = arr[index1];
     arr[index1] = arr[index2];
     arr[index2] = temp;
+  }
+
+  private static boolean isSorted(double[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) return false;
+    }
+    return true;
   }
 } // end class
