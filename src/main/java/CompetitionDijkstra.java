2aa0a9b95c971e42ecc924190052270cf6f9d698
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-03-24 00:12:08 +0000

    Add dijkstra's algorithm

--- src/main/java/CompetitionDijkstra.java
+++ src/main/java/CompetitionDijkstra.java
@@ -16,6 +16,7 @@
  */
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -44,7 +45,72 @@ public class CompetitionDijkstra {
     this.speedC = sC;
   }
 
-  private static class Graph {
+  /** @return int: minimum minutes that will pass before the three contestants can meet */
+  public int timeRequiredforCompetition() {
+    if (speedA < 0 || speedB < 0 || speedC < 0) return -1;
+    return 0;
+  }
+
+  /** @return an array of shortest path tables for each vertex */
+  private DijkstraEntry[][] generateAllShortestPaths() {
+    if (graph == null) return null;
+    DijkstraEntry[][] shortestPaths = new DijkstraEntry[graph.vertices.length][];
+    for (int i = 0; i < shortestPaths.length; i++) {
+      shortestPaths[i] = generateShortestPaths(i);
+    }
+    return shortestPaths;
+  }
+
+  /** @return the shortest path table generated using dijkstra's algorithm */
+  private DijkstraEntry[] generateShortestPaths(int startVertex) {
+    boolean[] seen = new boolean[graph.vertices.length];
+    Vertex[] vertices = graph.vertices;
+    DijkstraEntry[] shortestPaths = initialiseShortestPathsTable();
+    shortestPaths[startVertex].shortestDistance = 0;
+    Queue<DijkstraEntry> priorityQueue =
+        new PriorityQueue<>(Comparator.comparing(dijkstraEntry -> dijkstraEntry.shortestDistance));
+    priorityQueue.add(shortestPaths[startVertex]);
+    while (!priorityQueue.isEmpty()) {
+      DijkstraEntry currEntry = priorityQueue.poll();
+      Vertex curr = vertices[currEntry.vertexId];
+      seen[curr.label] = true;
+      for (Vertex adjacent : curr.adjacents) {
+        if (!seen[adjacent.label]) {
+          DijkstraEntry toInsert = shortestPaths[adjacent.label];
+          double calculatedDistance = currEntry.shortestDistance + curr.costs[adjacent.label];
+          if (calculatedDistance < toInsert.shortestDistance) {
+            toInsert.shortestDistance = calculatedDistance;
+            toInsert.prevId = curr.label;
+          }
+          priorityQueue.add(toInsert);
+        }
+      }
+    }
+    return shortestPaths;
+  }
+
+  /** @return the initialised shortest paths table according to dijkstra's algorithm */
+  private DijkstraEntry[] initialiseShortestPathsTable() {
+    DijkstraEntry[] shortestPaths = new DijkstraEntry[graph.vertices.length];
+    for (int i = 0; i < shortestPaths.length; i++) {
+      shortestPaths[i] = new DijkstraEntry(-1, Double.MAX_VALUE, i);
+    }
+    return shortestPaths;
+  }
+
+  private static class DijkstraEntry {
+    private int prevId;
+    private double shortestDistance;
+    private int vertexId;
+
+    private DijkstraEntry(int prevId, double shortestDistance, int vertex) {
+      this.prevId = prevId;
+      this.shortestDistance = shortestDistance;
+      this.vertexId = vertex;
+    }
+  }
+
+  private class Graph {
     private Vertex[] vertices;
 
     private Graph(String filename) {
@@ -58,22 +124,22 @@ public class CompetitionDijkstra {
         double distance = scanner.nextDouble();
         if (vertices[vertexFrom] == null) vertices[vertexFrom] = new Vertex(vertexFrom);
         if (vertices[vertexTo] == null) vertices[vertexTo] = new Vertex(vertexTo);
-        vertices[vertexFrom].connections.add(vertices[vertexTo]);
+        vertices[vertexFrom].adjacents.add(vertices[vertexTo]);
         vertices[vertexFrom].costs[vertexTo] = distance;
       }
       scanner.close();
     }
+  }
 
-    private class Vertex {
-      int label;
-      double[] costs;
-      List<Vertex> connections;
+  private class Vertex {
+    int label;
+    double[] costs;
+    List<Vertex> adjacents;
 
-      private Vertex(int label) {
-        this.label = label;
-        this.costs = new double[Graph.this.vertices.length];
-        this.connections = new ArrayList<>();
-      }
+    private Vertex(int label) {
+      this.label = label;
+      this.costs = new double[graph.vertices.length];
+      this.adjacents = new ArrayList<>();
     }
   }
 }
