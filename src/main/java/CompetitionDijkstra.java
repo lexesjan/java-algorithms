84deca3f3a6e0e7af8c52777965338e520c4069d
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-03-25 16:45:28 +0000

    Complete timeRequiredforCompetition function

--- src/main/java/CompetitionDijkstra.java
+++ src/main/java/CompetitionDijkstra.java
@@ -15,6 +15,8 @@
  * This class implements the competition using Dijkstra's algorithm
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -29,6 +31,8 @@ import java.util.Scanner;
  * @version HT 2020
  */
 public class CompetitionDijkstra {
+  private static final int KILOMETER_TO_METERS = 1000;
+
   private Graph graph;
   private int speedA;
   private int speedB;
@@ -39,7 +43,11 @@ public class CompetitionDijkstra {
    * @param sA, sB, sC: speeds for 3 contestants
    */
   public CompetitionDijkstra(String filename, int sA, int sB, int sC) {
-    this.graph = new Graph(filename);
+    try {
+      this.graph = new Graph(filename);
+    } catch (FileNotFoundException e) {
+      this.graph = null;
+    }
     this.speedA = sA;
     this.speedB = sB;
     this.speedC = sC;
@@ -47,15 +55,33 @@ public class CompetitionDijkstra {
 
   /** @return int: minimum minutes that will pass before the three contestants can meet */
   public int timeRequiredforCompetition() {
-    if (speedA < 0 || speedB < 0 || speedC < 0) return -1;
-    return 0;
+    int minSpeed = Math.min(Math.min(speedA, speedB), speedC);
+    double maxDistanceBetweenAnyTwoNodes = getMaxDistance();
+    if (minSpeed < 0 || maxDistanceBetweenAnyTwoNodes < 0) return -1;
+    return (int) Math.ceil((maxDistanceBetweenAnyTwoNodes * KILOMETER_TO_METERS) / minSpeed);
+  }
+
+  /** @return the distance of the two vertices which are the furthest from each other */
+  private double getMaxDistance() {
+    DijkstraEntry[][] allShortestPaths = generateAllShortestPaths();
+    if (allShortestPaths == null) return -1;
+    int numVertices = graph.numVertices;
+    double maxDistance = Double.MIN_VALUE;
+    for (int i = 0; i < numVertices; i++) {
+      for (int j = 0; j < numVertices; j++) {
+        if (i == j) continue;
+        double IJDistance = allShortestPaths[i][j].shortestDistance;
+        if (IJDistance != Double.POSITIVE_INFINITY) maxDistance = Math.max(maxDistance, IJDistance);
+      }
+    }
+    return maxDistance;
   }
 
   /** @return an array of shortest path tables for each vertex */
   private DijkstraEntry[][] generateAllShortestPaths() {
     if (graph == null) return null;
-    DijkstraEntry[][] shortestPaths = new DijkstraEntry[graph.vertices.length][];
-    for (int i = 0; i < shortestPaths.length; i++) {
+    DijkstraEntry[][] shortestPaths = new DijkstraEntry[graph.numVertices][];
+    for (int i = 0; i < graph.numVertices; i++) {
       shortestPaths[i] = generateShortestPaths(i);
     }
     return shortestPaths;
@@ -63,7 +89,7 @@ public class CompetitionDijkstra {
 
   /** @return the shortest path table generated using dijkstra's algorithm */
   private DijkstraEntry[] generateShortestPaths(int startVertex) {
-    boolean[] seen = new boolean[graph.vertices.length];
+    boolean[] seen = new boolean[graph.numVertices];
     Vertex[] vertices = graph.vertices;
     DijkstraEntry[] shortestPaths = initialiseShortestPathsTable();
     shortestPaths[startVertex].shortestDistance = 0;
@@ -74,7 +100,7 @@ public class CompetitionDijkstra {
       DijkstraEntry currEntry = priorityQueue.poll();
       Vertex curr = vertices[currEntry.vertexId];
       seen[curr.label] = true;
-      for (Vertex adjacent : curr.adjacents) {
+      for (Vertex adjacent : curr.adjacent) {
         if (!seen[adjacent.label]) {
           DijkstraEntry toInsert = shortestPaths[adjacent.label];
           double calculatedDistance = currEntry.shortestDistance + curr.costs[adjacent.label];
@@ -82,6 +108,7 @@ public class CompetitionDijkstra {
             toInsert.shortestDistance = calculatedDistance;
             toInsert.prevId = curr.label;
           }
+          priorityQueue.remove(toInsert);
           priorityQueue.add(toInsert);
         }
       }
@@ -91,9 +118,9 @@ public class CompetitionDijkstra {
 
   /** @return the initialised shortest paths table according to dijkstra's algorithm */
   private DijkstraEntry[] initialiseShortestPathsTable() {
-    DijkstraEntry[] shortestPaths = new DijkstraEntry[graph.vertices.length];
+    DijkstraEntry[] shortestPaths = new DijkstraEntry[graph.numVertices];
     for (int i = 0; i < shortestPaths.length; i++) {
-      shortestPaths[i] = new DijkstraEntry(-1, Double.MAX_VALUE, i);
+      shortestPaths[i] = new DijkstraEntry(-1, Double.POSITIVE_INFINITY, i);
     }
     return shortestPaths;
   }
@@ -110,36 +137,39 @@ public class CompetitionDijkstra {
     }
   }
 
-  private class Graph {
+  private static class Graph {
     private Vertex[] vertices;
+    private int numVertices;
 
-    private Graph(String filename) {
+    private Graph(String filename) throws FileNotFoundException {
       if (filename == null || "".equals(filename)) return;
-      Scanner scanner = new Scanner(filename);
-      this.vertices = new Vertex[scanner.nextInt()];
+      Scanner scanner = new Scanner(new File(filename));
+      this.numVertices = scanner.nextInt();
+      this.vertices = new Vertex[numVertices];
       int numEdges = scanner.nextInt();
       for (int i = 0; i < numEdges; i++) {
         int vertexFrom = scanner.nextInt();
         int vertexTo = scanner.nextInt();
         double distance = scanner.nextDouble();
-        if (vertices[vertexFrom] == null) vertices[vertexFrom] = new Vertex(vertexFrom);
-        if (vertices[vertexTo] == null) vertices[vertexTo] = new Vertex(vertexTo);
-        vertices[vertexFrom].adjacents.add(vertices[vertexTo]);
+        if (vertices[vertexFrom] == null)
+          vertices[vertexFrom] = new Vertex(vertexFrom, this.numVertices);
+        if (vertices[vertexTo] == null) vertices[vertexTo] = new Vertex(vertexTo, this.numVertices);
+        vertices[vertexFrom].adjacent.add(vertices[vertexTo]);
         vertices[vertexFrom].costs[vertexTo] = distance;
       }
       scanner.close();
     }
   }
 
-  private class Vertex {
+  private static class Vertex {
     int label;
     double[] costs;
-    List<Vertex> adjacents;
+    List<Vertex> adjacent;
 
-    private Vertex(int label) {
+    private Vertex(int label, int numVertices) {
       this.label = label;
-      this.costs = new double[graph.vertices.length];
-      this.adjacents = new ArrayList<>();
+      this.costs = new double[numVertices];
+      this.adjacent = new ArrayList<>();
     }
   }
 }
